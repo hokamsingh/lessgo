197adf5f32047ce03f9bf31e720b4e43e8f32162
hokamsingh <hokamsingh.mongoosetech@gmail.com>
2024-08-18 09:40:16 +0530

    feat: package module

--- pkg/lessgo/less.go
+++ pkg/lessgo/less.go
@@ -1,1 +1,43 @@
 package lessgo
+
+import (
+	"github.com/hokamsingh/lessgo/internal/core/config"
+	"github.com/hokamsingh/lessgo/internal/core/controller"
+	"github.com/hokamsingh/lessgo/internal/core/di"
+	"github.com/hokamsingh/lessgo/internal/core/middleware"
+	"github.com/hokamsingh/lessgo/internal/core/module"
+	"github.com/hokamsingh/lessgo/internal/core/router"
+	"github.com/hokamsingh/lessgo/internal/core/service"
+)
+
+type Controller = controller.Controller
+type BaseController = controller.BaseController
+
+func LoadConfig() config.Config {
+	config := config.LoadConfig()
+	return *config
+}
+
+type Container = di.Container
+
+func NewContainer() *Container {
+	return di.NewContainer()
+}
+
+type Middleware = middleware.Middleware
+type BaseMiddleware = middleware.BaseMiddleware
+
+type Module = module.Module
+
+func NewModule(name string, controllers []interface{}, services []interface{}) *Module {
+	return module.NewModule(name, controllers, services)
+}
+
+type Router = router.Router
+
+func NewRouter() *Router {
+	return router.NewRouter()
+}
+
+type BaseService = service.BaseService
+type Service = service.Service
