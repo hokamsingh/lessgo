5709819254e46a10c4411c56e7fc0ad66a510a6d
hokamsingh <hokamsingh.mongoosetech@gmail.com>
2024-08-18 13:30:37 +0530

    feat: expose all handlers and middlewares to root

--- pkg/lessgo/less.go
+++ pkg/lessgo/less.go
@@ -1,6 +1,8 @@
 package LessGo
 
 import (
+	"time"
+
 	"github.com/hokamsingh/lessgo/internal/core/config"
 	"github.com/hokamsingh/lessgo/internal/core/controller"
 	"github.com/hokamsingh/lessgo/internal/core/di"
@@ -10,34 +12,60 @@ import (
 	"github.com/hokamsingh/lessgo/internal/core/service"
 )
 
+// Expose core types
 type Controller = controller.Controller
 type BaseController = controller.BaseController
+type Container = di.Container
+type Middleware = middleware.Middleware
+type BaseMiddleware = middleware.BaseMiddleware
+type Module = module.Module
+type Router = router.Router
+type BaseService = service.BaseService
+type Service = service.Service
 
+// Expose middleware types and functions
+type CORSMiddleware = middleware.CORSMiddleware
+type RateLimiterMiddleware = middleware.RateLimiter
+type FileUploadMiddleware = middleware.FileUploadMiddleware
+
+// LoadConfig loads the configuration
 func LoadConfig() config.Config {
 	config := config.LoadConfig()
 	return *config
 }
 
-type Container = di.Container
-
+// NewContainer creates a new dependency injection container
 func NewContainer() *Container {
 	return di.NewContainer()
 }
 
-type Middleware = middleware.Middleware
-type BaseMiddleware = middleware.BaseMiddleware
-
-type Module = module.Module
-
+// NewModule creates a new module
 func NewModule(name string, controllers []interface{}, services []interface{}) *Module {
 	return module.NewModule(name, controllers, services)
 }
 
-type Router = router.Router
-
-func NewRouter(options []router.Option) *Router {
+// NewRouter creates a new Router with optional configuration
+func NewRouter(options ...router.Option) *Router {
 	return router.NewRouter(options...)
 }
 
-type BaseService = service.BaseService
-type Service = service.Service
+// Expose middleware options
+func WithCORS(options middleware.CORSOptions) router.Option {
+	return router.WithCORS(options)
+}
+
+func WithRateLimiter(limit int, interval time.Duration) router.Option {
+	return router.WithRateLimiter(limit, interval)
+}
+
+func WithJSONParser() router.Option {
+	return router.WithJSONParser()
+}
+
+func WithCookieParser() router.Option {
+	return router.WithCookieParser()
+}
+
+func WithFileUpload(uploadDir string) router.Option {
+	return router.WithFileUpload(uploadDir)
+}
